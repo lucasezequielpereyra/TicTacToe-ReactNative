b71fe6910629d84d4f96d8104df15a17632ff41d
Lucas Pereyra <lucas.pereyra5@mail.com>
2022-02-09 21:06:11 -0300

    Se revirtieron los cambios

--- src/components/molecules/header/index.js
+++ src/components/molecules/header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { Styles } from './style';
 
-const Header = ({ title }) => {
+const Header = () => {
   return (
     <View style={Styles.container}>
       <Text style={Styles.text}>tic tac toe</Text>
